28e9aef553af5c911c6e3faa895b4934c310f1fa
anhtran102 <anhtran102@gmail.com>
2016-03-14 15:00:30 +0700

    add

---
+++ web/script/controllers/products/products.js
@@ -0,0 +1,27 @@
+﻿'use strict';
+
+angular.module("myApp.controllers", []).controller("productController", ["$scope","$routeParams",'$http',
+    function ($scope, $routeParams, $http) {
+        var rootUrl = 'api/products';
+        var brandId = 0;
+        $scope.init = function () {
+            var url = "";
+            $scope.products = [];
+            if (!$routeParams.brandId) {
+                brandId = 0;
+            } else {
+                if(parseInt($routeParams.brandId, 10)){
+                    brandId = parseInt($routeParams.brandId, 10);
+                }               
+            }
+            url = rootUrl + "?brandId=" + brandId;
+            $http.get(url).then(
+                function (data) {
+                    $scope.products = data.data;
+                }, function (error) {
+                }
+               );
+        };
+
+        $scope.init();
+    }]);
---
+++ web/script/controllers/products/review.js
@@ -0,0 +1,28 @@
+﻿'use strict';
+
+angular.module("myApp.controllers", []).controller("reviewController", ["$scope", "$routeParams", '$http',
+    function ($scope, $routeParams, $http) {
+        var rootUrl = 'api/products';
+        $scope.review = {};
+        $scope.init = function () {
+            var url = "";
+            $scope.product = {};
+            var url = baseUrl + "/" + productId;
+            $http.get(url).then(
+            function (err) {
+
+            },
+            function (data) {
+                if (data.data) {
+                    $scope.product = data.data;
+                    $scope.review = {
+                        ProductId: $scope.product.productId,
+                        UserId: 1
+                    }
+                };
+            })
+            ;
+        };
+
+        $scope.init();
+    }]);
